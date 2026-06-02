fbd013b7f79b21ce35dfdfc8edbe9287a1dcb234
sangammmmmm <sangamsingh7555@gmail.com>
2026-06-02 21:55:02 +0530

    Create contains_duplicate.cpp
    Day 2 - Contains Duplicate solved

---
+++ Week_1/Day_2/contains_duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+
+        for(int x : nums)
+          {
+            if(st.find(x) != st.end())
+              return true;
+
+         st.insert(x);
+          }
+
+       return false; 
+    }
+};
