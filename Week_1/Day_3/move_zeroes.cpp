66949b38acd0b0d6bfa7453e2f1f6663939d7779
sangammmmmm <sangamsingh7555@gmail.com>
2026-06-03 22:17:52 +0530

    Create move_zeroes.cpp
    Day 3 - Move Zeroes solved

---
+++ Week_1/Day_3/move_zeroes.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+     int count = 0 ; 
+     for ( int i = 0 ; i < nums.size(); i ++){
+     if (nums[i]!= 0){
+        swap(nums[i],nums[count]);
+        count ++;
+        }
+     }
+    }
+};
